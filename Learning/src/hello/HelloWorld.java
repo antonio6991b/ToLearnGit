da4b81a2b21b78e91496c789e8fa0608eeb61232
Antony <antonio6991b@gmail.com>
2020-11-05 01:19:21 +0300

    create branch

--- Learning/src/hello/HelloWorld.java
+++ Learning/src/hello/HelloWorld.java
@@ -1,9 +1,10 @@
 package hello;
-
+import Library.java;
 public class HelloWorld {
 	public static void main(String[] args) {
 		String text = "Change two.";
 		System.out.println("HelloWorld "+text);
+		System.out.println(Library.square(5));
 		
 	}
 }
---
+++ Learning/src/hello/Library.java
@@ -0,0 +1,8 @@
+package hello;
+public class Library{
+	public static int square(int a) {
+		return a*a;
+	}
+}
+
+
