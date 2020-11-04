46589ed5b48ddb42ffc8f870aa5693031ab8a088
Antony <antonio6991b@gmail.com>
2020-11-05 00:58:30 +0300

    new text

--- Learning/src/hello/HelloWorld.java
+++ Learning/src/hello/HelloWorld.java
@@ -2,6 +2,8 @@ package hello;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		String text = "NewText";
+		System.out.println("HelloWorld "+text);
+		
 	}
 }
